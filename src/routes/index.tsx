ae0153e2887e6138abfd47d0ae77a3aef8d8997b
кαтнєєѕкυмαɾ <katheeskumar@outlook.com>
2024-05-22 10:33:29 +0530

    changed data to zpuli

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,12 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>ZPULI</Title>
+      <h1>ZPULI</h1>
+      <h3>Coming soon!</h3>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Powered by <a href="https://gitleaf.com" target="_blank">GitLeaf</a>
       </p>
     </main>
   );
